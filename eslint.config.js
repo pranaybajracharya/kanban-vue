a98df62a65e2fd5e7622da042c013aa4a230e88f
pranaybajracharya <pranaybajracharya00@gmail.com>
2025-10-16 21:28:32 +0200

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import vuetify from 'eslint-config-vuetify'
+
+export default vuetify({
+  antfu: true,
+  autoimports: true,
+})
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <KanbanBoard />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
